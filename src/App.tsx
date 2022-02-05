5ad05239001036bd6a38c7a91e8af56131507c65
domynikmv057 <domynikmv.057@gmail.com>
2022-02-04 23:58:05 -0300

    feat: add Theme light

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@mui/system";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { LightTheme } from "./shared/theme";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
